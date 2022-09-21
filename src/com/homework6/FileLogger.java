9e8acfa4f8a5a220d4fd2e011c72eee9709a5124
PimaUA <pimaua87@gmail.com>
2022-09-21 10:35:38 +0300

    fourth retake main

--- src/com/homework6/FileLogger.java
+++ src/com/homework6/FileLogger.java
@@ -7,19 +7,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FileLogger implements Logger {
-    private String newFileName;
+    private final String pathToFile;
     private final LoggingLevel currentLoggingLevel;
     private final long maxFileSize;
+    private String newFileName;
     private final String logEntriesFormat;
     private long lastModifiedFileLength;
 
     public FileLogger(FileLoggerConfiguration fileLoggerConfiguration) {
-        this.newFileName = fileLoggerConfiguration.getPresentNewFileName();
+        DateTimeFormatter fileTimeLogFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+        String formattedLogTime = fileTimeLogFormat.format(now);
+        this.pathToFile = fileLoggerConfiguration.getPathToFile();
         this.currentLoggingLevel = fileLoggerConfiguration.getCurrentLoggingLevel();
         this.maxFileSize = fileLoggerConfiguration.getMaxFileSize();
-        this.logEntriesFormat = fileLoggerConfiguration.getLogEntriesFormat();
+        this.newFileName = fileLoggerConfiguration.getNewFileName();
+        this.logEntriesFormat = String.format("%s %s Message:", formattedLogTime,
+                currentLoggingLevel);
     }
 
     //Main methods
@@ -44,7 +52,7 @@ public class FileLogger implements Logger {
     private void writeLogToFile(Path path, String content) {
         try {
             if (lastModifiedFileLength < maxFileSize) {
-                Files.write(path, content.getBytes(StandardCharsets.UTF_8),
+                Files.write(path, content.getBytes(StandardCharsets.UTF_8),         /*create & write to file*/
                         StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             } else {
                 String exceptionMessage = String.format("Max:%d Current:%d Path:%s",
@@ -57,9 +65,9 @@ public class FileLogger implements Logger {
         } catch (FileMaxSizeReachedException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
-            File file = new File(newFileName);
             try {
-                file.createNewFile();                         /*creation of new file after exception thrown*/
+                Files.write(path, content.getBytes(StandardCharsets.UTF_8),         /*create & write to file*/
+                        StandardOpenOption.CREATE, StandardOpenOption.APPEND);  /*after exception has been thrown*/
             } catch (IOException e2) {
                 e2.printStackTrace();
             }
@@ -67,26 +75,28 @@ public class FileLogger implements Logger {
     }
 
     private Path getNewFileName() {
-        Path directory = Paths.get(newFileName);
-        String dirPath = directory.getParent().toString();
-        final File dir = (new File(dirPath));
-        final boolean isDirectoryCreated = dir.mkdir();               /*creation of new directory at first start*/
+        final File dir = (new File(pathToFile));
+        final boolean isDirectoryCreated = dir.mkdir();   /*creation of new directory at first start*/
         File[] files = dir.listFiles();
         if (files != null) {
             if (isDirectoryCreated && files.length == 0) {
                 File file = new File(newFileName);
                 try {
-                    file.createNewFile();                         /*creation of new file at directory at first start*/
+                    file.createNewFile();             /*creation of new file at directory at first start*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             } else if (!isDirectoryCreated && files.length > 0) {
                 File lastModifiedFile = getLastModifiedFile(files);
-                if (getLastModifiedFileSize(lastModifiedFile) < maxFileSize) {
-                    newFileName = String.format("src/logsFolder/%s",           /* file name when directory*/
-                            lastModifiedFile.getName());                       /* exists and is not empty*/
+                if (getLastModifiedFileSize(lastModifiedFile) < maxFileSize) {     /* file name when directory*/
+                    newFileName = pathToFile + lastModifiedFile.getName();         /* exists and is not empty*/
                 } else if (getLastModifiedFileSize(lastModifiedFile) > maxFileSize) {
-                    return Paths.get(newFileName);                    /*file name when file size limit exceeded*/
+                    DateTimeFormatter fileNameTimeFormat =
+                            DateTimeFormatter.ofPattern("dd.MM.yyyy HH.mm.ss.SSS");
+                    LocalDateTime now = LocalDateTime.now();
+                    String formattedDate = fileNameTimeFormat.format(now);
+                    newFileName = String.format("src/logsFolder/Log_%s.txt",            /* file name when*/
+                            formattedDate);                                        /*file size limit exceeded*/
                 }
             }
         }
--- src/com/homework6/Main.java
+++ src/com/homework6/Main.java
@@ -2,15 +2,33 @@ package com.homework6;
 
 public class Main {
     public static void main(String[] args) {
-        FileLogger logger = new FileLogger(new FileLoggerConfiguration(
-                LoggingLevel.DEBUG, 100));
-        logger.debug("test debug");
-        //logger.info("test info");
-
         FileLoggerConfigurationLoader fileLoggerConfigurationLoader =
                 new FileLoggerConfigurationLoader();
-        FileLoggerConfiguration obj2 =
-                fileLoggerConfigurationLoader.load("src/load_config.txt");
-        //System.out.println(obj2);
+        FileLoggerConfiguration config = fileLoggerConfigurationLoader.load("src/load_config.txt");
+        FileLogger logger = new FileLogger(config);
+
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.debug("test debug");
+        logger.info("test info");
     }
 }
